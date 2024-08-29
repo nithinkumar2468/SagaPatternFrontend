f9c867e6302d41aaddda8a4cfff4d8af982be60b
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-08-29 12:49:46 +0530

    29/8/2024

--- src/Products/ProductsByHotel.js
+++ src/Products/ProductsByHotel.js
@@ -1,58 +1,61 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams, useNavigate ,Link} from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import Navbar from "../Login/Navbar";
 
 export default function ProductsByHotel() {
+  const { id } = useParams();
+  const [product, setProducts] = useState([]);
 
-    const { id } = useParams();
-    const [product, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8082/get/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    useEffect(() => {
-      fetch(`http://localhost:8082/get/products/${id}`)
-        .then((res) => res.json())
-        .then((data) => setProducts(data));
-    }, []);
-  
-    return (
-      <div>
-        <Navbar />
-        <div className="container d-flex justify-content-center mt-50 mb-50 py-20">
-          <div className="row">
-            <h1 style={{ textAlign: "center", color: "#cc3300" }}>{product.map((item)=>(<h1>{item.hotel.hotelName}</h1>))}</h1>
-            {product.map((item, index) => (
-              <div className="col-md-3 mt-4">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="card-img-actions">   
-                      <img
-                        src={`/images/${item.item.toLowerCase().replace(/\s/g, "")}.jpg`}
-                        style={{ width: "400px", height: "200px" }}
-                        alt=""
-                      />
-                    </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="container d-flex justify-content-center mt-50 mb-50 py-20">
+        <div className="row">
+          <h1 style={{ textAlign: "center", color: "#cc3300" }}>
+             {/*  <h1>{product[0].hotel.hotelName}</h1> */}
+          </h1>
+          {product.map((item, index) => (
+            <div className="col-md-3 mt-4">
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-img-actions">
+                    <img
+                      src={`/images/${item.item
+                        .toLowerCase()
+                        .replace(/\s/g, "")}.jpg`}
+                      style={{ width: "400px", height: "200px" }}
+                      alt=""
+                    />
                   </div>
-  
-                  <div className="card-body bg-light text-center">
-                    <div className="mb-2">
-                      <h3 className="font-weight-semibold mb-2">{item.item}</h3>
-  
-                      <h4>{item.hotel.hotelName}</h4>
-  
-                      <h4 className="mb-0 font-weight-semibold">₹{item.price}</h4>
-                      <Link
-                        className="btn btn-outline-primary mx-2"
-                        to={`/buynowproduct/${item.productId}`}
-                      >
-                        Buy Now
-                      </Link>
-                    </div>
+                </div>
+
+                <div className="card-body bg-light text-center">
+                  <div className="mb-2">
+                    <h3 className="font-weight-semibold mb-2">{item.item}</h3>
+
+                    <h4>{item.hotel.hotelName}</h4>
+
+                    <h4 className="mb-0 font-weight-semibold">₹{item.price}</h4>
+                    <Link
+                      className="btn btn-outline-primary mx-2"
+                      to={`/buynowproduct/${item.productId}`}
+                    >
+                      Buy Now
+                    </Link>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
